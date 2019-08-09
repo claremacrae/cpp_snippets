32e34793e8581734dc8131892c5261989965a0fa
Clare Macrae <github@cfmacrae.fastmail.co.uk>
2019-08-09 15:57:59 +0100

    f Add type_trait_is_base_of - only works with C++17

---
+++ samples/type_traits/type_trait_is_base_of.cpp
@@ -0,0 +1,52 @@
+#include <type_traits>
+#include <iostream>
+
+class Base
+{
+public:
+    [[nodiscard]] int in_base() const
+    {
+        return 1;
+    }
+};
+
+class Derived : public Base
+{
+public:
+    [[nodiscard]] int in_derived() const
+    {
+        return 2;
+    }
+};
+
+template<typename Class>
+void print_based_on_type(const Class& object)
+{
+    std::cout << std::boolalpha;
+    std::cout << "is Derived, or derived from Derived: " << std::is_base_of<Derived, Class>::value << '\n';
+    // https://stackoverflow.com/questions/32899259/does-c-11-support-template-class-reflection
+    // https://cmake.org/cmake/help/latest/manual/cmake-compile-features.7.html#manual:cmake-compile-features(7)
+    // warning: 'if constexpr' only available with -std=c++1z or -std=gnu++1z
+    if constexpr ( std::is_base_of<Derived, Class>::value )
+    {
+        std::cout << object.in_derived() << '\n';
+    }
+    else
+    {
+        std::cout << object.in_base() << '\n';
+    }
+}
+
+int main()
+{
+    {
+        Base b;
+        print_based_on_type(b);
+    }
+
+    {
+        Derived d;
+        print_based_on_type(d);
+    }
+
+}
