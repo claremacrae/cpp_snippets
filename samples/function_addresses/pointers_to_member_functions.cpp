16439e6928e68cd6e6a88a4cea35782ff76706a0
Clare Macrae <github@cfmacrae.fastmail.co.uk>
2020-03-04 11:34:40 +0000

    Add pointer to member & non-member function examples

---
+++ samples/function_addresses/pointers_to_member_functions.cpp
@@ -0,0 +1,49 @@
+#include "helper_classes.h"
+#include "catch2/catch.hpp"
+
+#if __cplusplus >= 201703L
+// For std::invoke
+#include <functional>
+#endif
+
+// Reference: https://isocpp.org/wiki/faq/pointers-to-members
+
+class Class
+{
+public:
+    int memberFunction(char c, int i) const
+    {
+        return i;
+    }
+
+    static int staticFunction(char c, int i)
+    {
+        return i;
+    }
+};
+
+// For motivation, see https://isocpp.org/wiki/faq/pointers-to-members#macro-for-ptr-to-memfn
+#define CALL_MEMBER_FN(object, ptrToMember) ((object).*(ptrToMember))
+
+TEST_CASE("Address of const member function compiles")
+{
+    typedef int (Class::*ClassMemFn)(char x, int y) const;
+    ClassMemFn memFn = &Class::memberFunction;
+
+    Class c;
+
+    // Pre C++ 17
+    CHECK(42 == CALL_MEMBER_FN(c, memFn)('x', 42));
+
+    // C++17:
+#if __cplusplus >= 201703L
+    CHECK(42 == std::invoke(memFn, c, 'x', 42));
+#endif
+}
+
+TEST_CASE("Address of static member function compiles")
+{
+    typedef int (*ClassStaticFn)(char x, int y);
+    ClassStaticFn staticFn = &Class::staticFunction;
+    CHECK(42 == (*staticFn)('x', 42));
+}
